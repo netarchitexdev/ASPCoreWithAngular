79446ef095eb2c542b1fa368fa41450eba64ff9b
netarchitexdev <netarchitex.dev@gmail.com>
2018-09-19 18:41:59 -0500

    Temp added start of lazy loading functionality.

--- ASPCoreWithAngular/ClientApp/src/app/roles/roles.component.ts
+++ ASPCoreWithAngular/ClientApp/src/app/roles/roles.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RoleService, IRole } from '../services/role.service';
-//import { error } from 'protractor';
+import { LazyLoadEvent } from 'primeng/primeng';
 
 @Component({
   selector: 'app-roles',
@@ -8,17 +8,23 @@ import { RoleService, IRole } from '../services/role.service';
 })
 export class RolesComponent implements OnInit {
 
-  role: IRole;
+  //role: IRole;
 
-  selectedRole: IRole;
+  //selectedRole: IRole;
 
-  newRole: boolean;
+  //newRole: boolean;
 
   public roles: IRole[] = [];
 
+  //public roles: Array<any>;
+
   public cols: any[];
 
-  //public roles: Array<any>;
+  public loading: boolean;
+
+  public datasource: IRole[];
+
+  public totalRecords: number;  
 
   constructor(private roleService: RoleService) {
   }
@@ -30,16 +36,37 @@ export class RolesComponent implements OnInit {
       { field: 'roleName', header: 'Name' }
     ];
 
+    //this.roleService.getAll()
+    //  .subscribe((data: any) => {
+    //    this.roles = data;
+    //  });
+
     this.roleService.getAll()
       .subscribe((data: any) => {
-        this.roles = data;
+        this.datasource = data;
+        this.totalRecords = this.datasource.length;
       });
 
-    //this.roleService.get()
-    //  .subscribe(
-    //    data => this.roles = data,
-    //    error => console.log("error in component")
-    //  );
+    this.loading = true;
+  }
+
+  loadRolesLazy(event: LazyLoadEvent) {
+    this.loading = true;
+
+    //in a real application, make a remote request to load data using state metadata from event
+    //event.first = First row offset
+    //event.rows = Number of rows per page
+    //event.sortField = Field name to sort with
+    //event.sortOrder = Sort order as number, 1 for asc and -1 for dec
+    //filters: FilterMetadata object having field as key and filter value, filter matchMode as value
+
+    //imitate db connection over a network
+    setTimeout(() => {
+      if (this.datasource) {
+        this.roles = this.datasource.slice(event.first, (event.first + event.rows));
+        this.loading = false;
+      }
+    }, 1000);
   }
 
 }
