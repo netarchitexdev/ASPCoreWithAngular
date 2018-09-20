63322e3605ec222bd55665bf9a36ff5038616b16
netarchitexdev <netarchitex.dev@gmail.com>
2018-09-19 21:16:09 -0500

    Cleanup.  Everything working so far!

--- ASPCoreWithAngular/ClientApp/src/app/roles/roles.component.ts
+++ ASPCoreWithAngular/ClientApp/src/app/roles/roles.component.ts
@@ -7,12 +7,6 @@ import { RoleService, IRole } from '../services/role.service';
 })
 export class RolesComponent implements OnInit {
 
-  role: IRole;
-
-  selectedRole: IRole;
-
-  newRole: boolean;
-
   roles: IRole[] = [];
 
   cols: any[];
@@ -27,35 +21,10 @@ export class RolesComponent implements OnInit {
       { field: 'roleName', header: 'Name' }
     ];
 
-    //this.roleService.getAll()
-    //  .subscribe((data: any) => {
-    //    this.roles = data;
-    //  });
-
     this.roleService.getAll()
       .subscribe((data: any) => {
         this.roles = data;
-      }
-      , () => console.log('Finally'));
+      });
   }
-
-  //loadRolesLazy(event: LazyLoadEvent) {
-  //  this.loading = true;
-
-  //  //in a real application, make a remote request to load data using state metadata from event
-  //  //event.first = First row offset
-  //  //event.rows = Number of rows per page
-  //  //event.sortField = Field name to sort with
-  //  //event.sortOrder = Sort order as number, 1 for asc and -1 for dec
-  //  //filters: FilterMetadata object having field as key and filter value, filter matchMode as value
-
-  //  //imitate db connection over a network
-  //  setTimeout(() => {
-  //    if (this.datasource) {
-  //      this.roles = this.datasource.slice(event.first, (event.first + event.rows));
-  //      this.loading = false;
-  //    }
-  //  }, 1000);
-  //}
 
 }
